84d1764263cfa4c3dcde145296bd77cd77f1429a
henvisi-1994 <henvisi_1994@hotmail.com>
2020-10-05 22:39:08 -0500

    conexion a la bd

---
+++ redsocial-api/src/controllers/conexionDB.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'ec2-204-236-228-169.compute-1.amazonaws.com',
+    user: 'xbklbxypnbfjaa',
+    password: 'c1e66dc38e7316ef998f6783ad43a08446b995e72ead22a64703fe6bfe9231b4',
+    database: 'de6l3lv5na4bng',
+    port: '5432'
+});
+module.exports=pool;
