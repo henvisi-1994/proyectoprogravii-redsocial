63c633d478c371f1727b2c24998af35c9c116238
Gillgard <ricardoramonramon@gmail.com>
2020-11-02 17:24:19 -0500

    Expresiones regulares en registro

--- redsocial-frontend/src/app/componets/usuario/registro/registro.component.ts
+++ redsocial-frontend/src/app/componets/usuario/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { UsuarioService } from './../../../services/usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/modelos/Usuario';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -9,6 +10,18 @@ import { Usuario } from 'src/app/modelos/Usuario';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+
+  public registroForm: FormGroup;
+
+  //Regex para el formato de correo con la siguiente estructura: xxxxxxx@xxxx.xxx
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //Regex para el ingreso de contraseñas de 8 a 15 caracteres
+  private passPattern: any = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+  //regex para nombres y apellidos, incluye letras y espacios
+  private namePattern: any = /^[A-Za-zÁÉÍÓÚñáéíóúÑ ]+$/;
+  //Regex para el nombre de usuario, incluye cualquier caracter menos espacio
+  private userPattern: any = /^[a-z0-9_!.-]{3,16}$/;
+
   usuario: Usuario = {  id_usuario: 0,
     nombres_user: '',
     apellidos_user: '',
@@ -21,10 +34,29 @@ export class RegistroComponent implements OnInit {
     telefono: '',
     id_genero: 0 };
     generos = [];
-  constructor(private usuarioService: UsuarioService,
-              private router: Router, ) { }
-  ngOnInit(): void {
+
+  constructor(private usuarioService: UsuarioService, private fb: FormBuilder ) {
+               }
+  
+  get email() { return this.registroForm.get('email'); }
+  get nombres() { return this.registroForm.get('nombres'); }
+  get apellidos() { return this.registroForm.get('apellidos'); }
+  get username() { return this.registroForm.get('username'); }
+  get contrasena() { return this.registroForm.get('contrasena'); }
+  get fecha_nacimiento() { return this.registroForm.get('fecha_nacimiento'); }
+  get genero() { return this.registroForm.get('genero'); }
+  
+   ngOnInit(): void {
     this.getGeneros();
+    this.registroForm = this.fb.group({
+      email: new FormControl('',  [Validators.required, Validators.pattern(this.emailPattern)]),
+      nombres: new FormControl('', [Validators.required, Validators.pattern(this.namePattern)]),
+      apellidos: new FormControl('', [Validators.required, Validators.pattern(this.namePattern)]),
+      fecha_nacimiento: new FormControl(''),
+      username: new FormControl('', [Validators.required, Validators.pattern(this.userPattern)]),
+      contrasena: new FormControl('', [Validators.required, Validators.pattern(this.passPattern)]),
+      genero: new FormControl('')
+    });
   }
   // Registra usuario
   // tslint:disable-next-line: typedef
@@ -37,7 +69,6 @@ export class RegistroComponent implements OnInit {
         this.limpiar();
       },
       err => {
-
       }
     );
   }
@@ -64,5 +95,4 @@ export class RegistroComponent implements OnInit {
       }, err => {}
     );
   }
-
 }
