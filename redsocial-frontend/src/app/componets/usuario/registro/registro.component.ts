b7411e49b9a60bcaeebb8c0055d4b3339dc087da
FrankWN29 <44509452+FrankWN29@users.noreply.github.com>
2020-11-05 22:52:50 -0500

    redsocial v0.0.3 (validacion de formulario registro de usuario)

--- redsocial-frontend/src/app/componets/usuario/registro/registro.component.ts
+++ redsocial-frontend/src/app/componets/usuario/registro/registro.component.ts
@@ -2,7 +2,6 @@ import { UsuarioService } from './../../../services/usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/modelos/Usuario';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -10,18 +9,6 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  public registroForm: FormGroup;
-
-  //Regex para el formato de correo con la siguiente estructura: xxxxxxx@xxxx.xxx
-  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //Regex para el ingreso de contraseñas de 8 a 15 caracteres
-  private passPattern: any = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
-  //regex para nombres y apellidos, incluye letras y espacios
-  private namePattern: any = /^[A-Za-zÁÉÍÓÚñáéíóúÑ ]+$/;
-  //Regex para el nombre de usuario, incluye cualquier caracter menos espacio
-  private userPattern: any = /^[a-z0-9_!.-]{3,16}$/;
-
   usuario: Usuario = {  id_usuario: 0,
     nombres_user: '',
     apellidos_user: '',
@@ -41,15 +28,6 @@ export class RegistroComponent implements OnInit {
               private router: Router, ) { }
   ngOnInit(): void {
     this.getGeneros();
-    this.registroForm = this.fb.group({
-      email: new FormControl('',  [Validators.required, Validators.pattern(this.emailPattern)]),
-      nombres: new FormControl('', [Validators.required, Validators.pattern(this.namePattern)]),
-      apellidos: new FormControl('', [Validators.required, Validators.pattern(this.namePattern)]),
-      fecha_nacimiento: new FormControl(''),
-      username: new FormControl('', [Validators.required, Validators.pattern(this.userPattern)]),
-      contrasena: new FormControl('', [Validators.required, Validators.pattern(this.passPattern)]),
-      genero: new FormControl('')
-    });
   }
   // Registra usuario
   // tslint:disable-next-line: typedef
@@ -111,4 +89,5 @@ export class RegistroComponent implements OnInit {
       }, err => {}
     );
   }
+
 }
