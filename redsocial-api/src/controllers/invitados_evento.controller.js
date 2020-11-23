e448faf6037f40b64f8d0909f34f2e572501cae5
Marvin Valarezo <marvinv572@gmail.com>
2020-11-22 20:13:05 -0500

    Carga Backend2 Marvin

--- redsocial-api/src/controllers/invitados_evento.controller.js
+++ redsocial-api/src/controllers/invitados_evento.controller.js
@@ -8,9 +8,16 @@ invitados_evento.getinvitados = async (req, res) => {
 }
 
 invitados_evento.getinvitado = async (req, res) => {
-    const id = req.params.id_invitado;
-    const response = await conexion.query(`select  * from invitados_evento INNER JOIN evento on invitados_evento.id_evento = evento.id_evento INNER JOIN usuario on invitados_evento.id_usuario= usuario.id_usuario  where invitados_evento.id_invitado = ${id} ORDER by invitados_evento.id_invitado DESC`);
+    const id = req.params.id_usuario;
+    const response = await conexion.query(`select  * from invitados_evento INNER JOIN evento on invitados_evento.id_evento = evento.id_evento INNER JOIN usuario on invitados_evento.id_usuario= usuario.id_usuario  where invitados_evento.id_usuario = ${id} ORDER by invitados_evento.id_invitado DESC`);
     res.status(200).json(response.rows);
+}
+invitados_evento.getestadistica = async (req, res) => {
+    const id = req.params.id_evento;
+    const asistencia = await conexion.query(`select count(estado_evento) from invitados_evento where estado_evento ='asistir' and id_evento = ${id}`);
+    const tasistencia = await conexion.query(`select count(estado_evento) from invitados_evento where estado_evento ='talvez asista' and id_evento = ${id}`);
+    const nasistencia = await conexion.query(`select count(estado_evento) from invitados_evento where estado_evento ='no asistire' and id_evento = ${id}`);
+    res.status(200).json({asistire: asistencia.rows[0].count, talvez_asista: tasistencia.rows[0].count, no_asistire: nasistencia.rows[0].count});
 }
 
 invitados_evento.registro = async (req, res) => {
@@ -29,6 +36,14 @@ invitados_evento.update = async (req, res) => {
     const id = req.params.id_invitado;
     const {id_evento, estado_evento, id_usuario} = req.body;
     let query = `UPDATE invitados_evento SET id_evento='${id_evento}', estado_evento='${estado_evento}', id_usuario='${id_usuario}' WHERE id_invitado = ${id}`;
+    await conexion.query(query);
+    res.json('Invitados Actualizado con exito');
+}
+//Actualiza datos de eventos mediante id
+invitados_evento.cambiar_estado = async (req, res) => {
+    const id = req.params.id_usuario;
+    const {estado_evento} = req.body;
+    let query = `UPDATE invitados_evento SET  estado_evento='${estado_evento}' WHERE id_usuario = ${id}`;
     await conexion.query(query);
     res.json('Invitados Actualizado con exito');
 }
