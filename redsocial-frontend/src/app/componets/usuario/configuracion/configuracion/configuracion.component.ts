d3156bc79b4ef53090a8c320a6900a0e76493b8a
FrankWN29 <44509452+FrankWN29@users.noreply.github.com>
2020-12-02 14:20:28 -0500

    modificaciones-generales

--- redsocial-frontend/src/app/componets/usuario/configuracion/configuracion/configuracion.component.ts
+++ redsocial-frontend/src/app/componets/usuario/configuracion/configuracion/configuracion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-configuracion',
@@ -10,6 +11,17 @@ export class ConfiguracionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  cambiarTema(event){
+    let evento=event.target.checked;
+    let originalCSS={ "background-color": '#edf2f6' };
+    let nuevoCSS = { "background": '#1f1f1f', "color" : '#f1eded' };
+    if(evento){
+      $("body").css(nuevoCSS);
+    }
+    else{
+      $("body").css(originalCSS );
+    }
   }
 
 }
